1776be8ccb44a36dbae292d5ebd5b5222310f245
AmetoX <67999655+AmetoX@users.noreply.github.com>
2024-07-25 11:07:31 +0300

    added hud and small changeings

--- Engine/world/game_manager.gd
+++ Engine/world/game_manager.gd
@@ -2,7 +2,11 @@ extends Node
 
 var score = 0
 @onready var score_label = $ScoreLabel
+#@onready var player_score = $Hud/playerScore
+@onready var coin_count = $Hud/CoinCount
 
 func add_point():
 	score += 1
 	score_label.text = "Coins collected: " + str(score)
+	#player_score.text = "Coins: " + str(score)
+	coin_count.text = "Coins: " + str(score)
