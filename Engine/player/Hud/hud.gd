b026d1bce855fdbf411371e511f4c9b21f2a469a
AmetoX <67999655+AmetoX@users.noreply.github.com>
2024-07-26 15:30:27 +0300

    useless piece of shit godot

--- Engine/player/Hud/hud.gd
+++ Engine/player/Hud/hud.gd
@@ -2,7 +2,7 @@ extends CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$CoinCount.text = "coins: 0"
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
